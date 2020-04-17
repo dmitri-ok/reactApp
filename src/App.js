c1ac318edd8c82b08bea160e149992103bf17b63
dmitri-ok <dmitriok@gmail.com>
2020-04-17 21:40:30 +0300

    rename

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Main from './Main';
-import Footer from './Footer';
+import Footer from 'Footer';
 function App() {
   return (
     <div className="App">
