ce8c087287c467f0036d34c0defd1820740775e7
dmitri-ok <dmitriok@gmail.com>
2020-04-17 19:36:53 +0300

    add some dirs

---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Header.css';
+
+function App() {
+  return <footer>Copyright</footer>;
+}
+export default App;
---
+++ src/Main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Header.css';
+
+function App() {
+  return (
+    <main>
+      <article>
+        <header>
+          <h1>Article 1</h1>
+          <h2>Article 2</h2>
+        </header>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          nesciunt qui sint illum? Aspernatur laudantium quas mollitia, a quo
+          eligendi optio blanditiis odio illum soluta ipsam, eum deleniti dolore
+          architecto.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          nesciunt qui sint illum? Aspernatur laudantium quas mollitia, a quo
+          eligendi optio blanditiis odio illum soluta ipsam, eum deleniti dolore
+          architecto.
+        </p>
+      </article>
+    </main>
+  );
+}
+export default App;
